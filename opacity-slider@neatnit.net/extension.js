1792ff22ac21f019af6f5be9b840c102ff60de09
NeatNit <NeatNit@gmail.com>
2023-02-10 02:21:50 +0200

    finish basically

--- opacity-slider@neatnit.net/extension.js
+++ opacity-slider@neatnit.net/extension.js
@@ -1,75 +1,134 @@
+"use strict";
+
 const PopupMenu = imports.ui.popupMenu;
 const WindowMenu = imports.ui.windowMenu;
 
-let isEnabled = false;
-let _buildMenuBase, my_buildMenu;
+let isEnabled = false, isInstalled = false;
+let base_buildMenu, my_buildMenu;
+let uuid = "opacity-slider@unknown.uuid"
 
 // implement opacity slider that does the same thing as the scroll action:
 // https://github.com/linuxmint/muffin/blob/e78b8335f3113c38b7611193ae55ff3e14820961/src/core/window.c#L8803-L8817
-//
-// namely:
-// 1. set opacity
-// 2. call meta_compositor_window_opacity_changed?????
 
+function init(metadata) {
+    uuid = metadata.uuid;
+}
 
+let log = function(text) {
+    global.log("[" + uuid + "]: " + text);
+}
+
+let logW = function(text) {
+    global.logWarning("[" + uuid + "]: " + text);
+}
+
+// add a slider 
 function addSlider(to_menu, value, callback) {
     // variant of WindowMenu.prototype.addAction, but that adds a slider instead of a MnemonicLeftOrnamentedMenuItem
     let menuItem = new PopupMenu.PopupSliderMenuItem(value);
-    to_menu.addMenuItem(menuItem);
+    to_menu.addMenuItem(menuItem, to_menu.numMenuItems - 2);
     
-    menuItem.connect('value-changed', (o, v) => callback(v) ); // callback receives value as argument
+    menuItem.connect('value-changed', callback );
     
-    // this._items.push(menuItem); // this is used for mnemonics, to uncomment this line you must extend PopupSliderMenuItem and implement stuff
+    // this._items is used for mnemonics
+    // before you uncomment the next line you must extend PopupSliderMenuItem and implement stuff
+    // see windowMenu.js
+    //this._items.push(menuItem); 
     
     return menuItem;
 }
 
-function init(metadata) {
+const opacity_max = 255;
+const opacity_min = 26;
+
+function sliderValueFromOpacity(opacity) {
+    return Math.max(0, Math.min(1, (opacity - opacity_min) / (opacity_max - opacity_min)));
+}
+
+function opacityFromSliderValue(value) {
+    return Math.max(0, Math.min(255, Math.round( (value * (opacity_max-opacity_min) ) + opacity_min )));
+}
+
+function install() {
     // install ourselves by diverting the WindowMenu internal function
     // hacky as hell!
     
-    global.log("opacity-slider@neatnit.net: overriding WindowMenu.prototype._buildMenu!")
-    _buildMenuBase = WindowMenu.WindowMenu.prototype._buildMenu;
-    WindowMenu.WindowMenu.prototype._buildMenu = function(window, ...args) {
+    base_buildMenu = WindowMenu.WindowMenu.prototype._buildMenu;
+    my_buildMenu = function(window, ...args) {
         // Creating a right-click menu
         
-        // Create default menu
-        let retval = _buildMenuBase.call(this, window, ...args);
+        // Build the normal menu
+        const retval = base_buildMenu.call(this, window, ...args);
         
         // If we're enabled, create our extra slider 
         if (isEnabled) {
-            this.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+            // Add separator
+            // The position this.numMenuItems-2 adds the item before the last item (Close)
+            this.addMenuItem(new PopupMenu.PopupSeparatorMenuItem(), this.numMenuItems - 2);
             
-            let opacity_sub = new MnemonicSubMenuMenuItem(_("O_pacity"));
-            this.addMenuItem(opacity_sub);
-            this._items.push(opacity_sub);
+            // Add label
+            const label = new PopupMenu.PopupMenuItem(_("Opacity") + ": ???");
+            this.addMenuItem(label, this.numMenuItems - 2);
             
-            let slider = addSlider.call(this, opacity_sub, window.get_opacity()/255, (value) => {                
+            // Function to update the label's text
+            function updateLabel(opacity) {
+                label.setLabel(_("Opacity") + ": " + Math.floor(100*opacity/255).toString() + "%")
+            }
+            
+            // Use current opacity to set label text
+            const initial_opacity = window.get_opacity();
+            updateLabel(initial_opacity)
+            
+            // Create the slider
+            addSlider.call(this, this, sliderValueFromOpacity(initial_opacity), (sl, value) => {                
                 // convert the value range (0 to 1) to an acceptable opacity
-                const opacity_max = 255;
-                const opacity_min = 30;
                 
-                const opacity = Math.round( (value * (opacity_max-opacity_min) ) + opacity_min );
+                const opacity = opacityFromSliderValue(value);
                 
                 window.set_opacity(opacity);
+                
+                updateLabel(opacity);
             } );
-
-            
         }
         
         return retval
     }
     
-    my_buildMenu = WindowMenu.WindowMenu.prototype._buildMenu;
+    log("overriding WindowMenu.prototype._buildMenu")
+    WindowMenu.WindowMenu.prototype._buildMenu = my_buildMenu;
+    isInstalled = true;
+}
+
+function tryUninstall() {
+    // Try to revert the method we overrode to what it was before
+    // We can only do this if no one else overrode it after us
+    // (unless they also cleaned up after themselves like we're doing here)
+    if (WindowMenu.WindowMenu.prototype._buildMenu == my_buildMenu) {
+        // We are clear to undo!
+        log("reverting WindowMenu.prototype._buildMenu to former value")
+        WindowMenu.WindowMenu.prototype._buildMenu = base_buildMenu;
+        isInstalled = false;
+        
+        // allow garbage collection
+        base_buildMenu = undefined;
+        my_buildMenu = undefined;
+    } else {
+        logW("cannot revert WindowMenu.prototype._buildMenu: it has been overridden elsewhere")
+    }
 }
 
 function enable() {
     isEnabled = true;
+    
+    if (!isInstalled) {
+        install();
+    }
 }
 
 function disable() {
-    global.logWarning("opacity slider: disable")
     isEnabled = false;
     
-    
+    if (isInstalled) {
+        tryUninstall();
+    }
 }
